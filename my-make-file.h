067b9aa66420ae5c0750bfe222caf962a276f69a
sohinim98 <35234448+sohinim98@users.noreply.github.com>
2018-01-23 12:43:57 -0500

    Create my-make-file.h
    Makefile source code

---
+++ my-make-file.h
@@ -0,0 +1,12 @@
+main compiler
+CC := gcc
+
+TARGET1 := ProjectCcode
+all: $(TARGET1)
+
+$(TARGET1): 
+    @echo "Compiling C program"
+    $(CC) $(CFLAGS) $(TARGET1).c -o $(TARGET1) $(LDFLAGS) -l$(LIB)
+
+clean:
+    @rm -rf $(TARGET1) $(TARGET2)
